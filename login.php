2d0d9534264b84c24d34a78d5dee38cc6aadd16d
JavierLH <abislopeznande@gmail.com>
2022-12-07 22:08:37 -0600

    estilos

--- login.php
+++ login.php
@@ -77,7 +77,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 }
 ?>
  
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -87,27 +87,55 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         body{ font: 14px sans-serif; }
         .wrapper{ width: 350px; padding: 20px; }
     </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <div class="wrapper">
-        <h2>Iniciar sesion</h2>
-        <p>Introduce los datos para iniciar sesion</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Nombre de usuario</label>
-                <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Contraseña</label>
-                <input type="password" name="password" class="form-control">
-                <span class="help-block"><?php echo $password_err; ?></span>
+   
+
+       
+   
+    
+<section class="vh-100 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
+
+            <div class="mb-md-5 mt-md-4 pb-5">
+
+            <h2>Iniciar sesión</h2>
+             <p>Llantas Max</p>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                    <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                        <label for="formGroupExampleInput" class="form-label">Nombre de usuario</label>
+                        <input type="text" name="username"class="form-control" placeholder="Escribe tu nombre de usuario" value="<?php echo $username; ?>">
+                        <span class="help-block"><?php echo $username_err; ?></span>
+                    </div>    
+                    <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                        <label>Contraseña</label>
+                        <input type="password" name="password" class="form-control" placeholder="Escribe tu contraseña">
+                        <span class="help-block"><?php echo $password_err; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-outline-light btn-lg px-5" value="Entrar">
+                    </div>
+                    <p>no tienes cuenta? <a href="register.php">Registrate</a>.</p>
+                    
+                </form>
+
+              <div class="d-flex justify-content-center text-center mt-4 pt-1">
+              <button type="button" class="btn btn-link btn-floating mx-1">
+                     <i class="fab fa-facebook-f" ></i>
+                     <a href="http://192.168.100.93/">Registrate con Facebook</a>
+                     </button>
+              </div>
             </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Entrar">
-            </div>
-            <p>no tienes cuenta? <a href="register.php">Registrate</a>.</p>
-        </form>
-    </div>    
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 </body>
 </html>
