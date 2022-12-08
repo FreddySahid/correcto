fa350d8263aef56dccbf6e32ee0a288bcf3da248
JavierLH <abislopeznande@gmail.com>
2022-12-07 22:14:17 -0600

    Administrador

--- administrador.php
+++ administrador.php
@@ -17,20 +17,56 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 }
 ?>
  
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Bienvenido</title>
     <link rel="stylesheet" href="bootstrap337/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <style type="text/css">
         body{ font: 14px sans-serif; text-align: center; }
     </style>
 </head>
 <body>
-    <div class="page-header">
-        <h1>Hola, <b><?php echo htmlspecialchars($_SESSION['username']); ?></b>. Bienvenido a tu sitio, administrador. Tu usuario es de tipo <b><?php echo htmlspecialchars($_SESSION['usertype']); ?></b></h1>
+   
+
+    <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Bienvenido<b><?php echo htmlspecialchars($_SESSION['username']); ?></b>. a Llantas Max. Tu usuario es de tipo <b><?php echo htmlspecialchars($_SESSION['usertype']); ?></b></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+        <a href="crud_tamales.php" class="nav-link">Ir a realizar venta</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">En construcción/ no disponible</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          En construcción/ no disponible
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Buscar</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="No disponible" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+      </form>
     </div>
-    <p><a href="https://6f56-148-226-28-247.ngrok.io/login.html.php" class="btn btn-success">Ir a los tamales</a></p>
+  </div>
+</nav>
 </body>
 </html>
